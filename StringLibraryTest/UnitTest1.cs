4b536beec0bfedd732259661348df6847efac7bd
ilvna <ilvxna@hotmail.com>
2023-08-09 10:55:27 +0800

    add test

--- StringLibraryTest/UnitTest1.cs
+++ StringLibraryTest/UnitTest1.cs
@@ -1,10 +1,13 @@
-namespace StringLibraryTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestClass]
-public class UnitTest1
+namespace StringLibraryTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class UnitTest1
     {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
     }
 }
