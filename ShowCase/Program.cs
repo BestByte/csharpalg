4e482e0399e9a505e9d20ff0d5e829049e9fa1cd
ilvna <ilvxna@hotmail.com>
2023-08-09 16:01:13 +0800

    add main

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using UtilityLibraries;
 public class Program
 {
    public static void Main(string[] args)
@@ -8,6 +8,35 @@ public class Program
        QuickSortArray(arr, 0, arr.Length - 1);
        Console.WriteLine("Sorted array:");
        PrintArray(arr);
+       int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " +
+                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
    }
 
    public static void QuickSortArray(int[] arr, int low, int high)
@@ -51,4 +80,6 @@ public class Program
        }
        Console.WriteLine();
    }
+
+   
 }
