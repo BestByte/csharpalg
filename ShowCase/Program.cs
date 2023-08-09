fb21682c0535c56fe06933ecaf9132dc5dffd63d
ilvna <ilvxna@hotmail.com>
2023-08-09 16:14:02 +0800

    add program:art:

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using UtilityLibraries;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 public class Program
 {
    public static void Main(string[] args)
@@ -8,35 +11,7 @@ public class Program
        QuickSortArray(arr, 0, arr.Length - 1);
        Console.WriteLine("Sorted array:");
        PrintArray(arr);
-       int row = 0;
-
-        do
-        {
-            if (row == 0 || row >= 25)
-                ResetConsole();
-
-            string? input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input)) break;
-            Console.WriteLine($"Input: {input}");
-            Console.WriteLine("Begins with uppercase? " +
-                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
-            Console.WriteLine();
-            row += 4;
-        } while (true);
-        return;
-
-        // Declare a ResetConsole local method
-        void ResetConsole()
-        {
-            if (row > 0)
-            {
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-            }
-            Console.Clear();
-            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
-            row = 3;
-        }
+      
    }
 
    public static void QuickSortArray(int[] arr, int low, int high)
